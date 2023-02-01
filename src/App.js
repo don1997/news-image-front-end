3ad3d5d7129870dbecfc0a04f79bb010d78de350
don1997 <donald_j97@hotmail.com>
2023-02-01 10:29:30 -0500

    change

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
+
+    
     <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
     <p className="text-3xl text-gray-700 font-bold mb-5">
       Welcome!
