4ce419d031683d8887b79943ddd27786f91094dc
don1997 <donald_j97@hotmail.com>
2023-02-01 15:34:33 -0500

    fix11

--- src/App.js
+++ src/App.js
@@ -11,35 +11,26 @@ const NewsList = () => {
 
   }, []);
 
-  const elements = [];
-
-  news.forEach(article => {
-    elements.push(
-      <div class="relative mb-4 before:content-[''] before:rounded-md before:absolute before:inset-0 before:bg-black before:bg-opacity-20">
-            <img class="w-full h-full rounded-md" src={article.urlToImage}/>
-            <div class="absolute inset-0 bg-black left-0 top-0 flex flex-col items-center justify-center opacity-0 hover:opacity-60 bg-opacity-90 duration-300">
-              <div class="relative">
-                <a class="test__link absolute inset-0" target="_blank" href={article.url}></a>
-                <h1 class="test__title text-orange-300 text-xs md:text-sm lg:text-xl font-bold mb-3">{article.title}</h1>
-              </div>
-
-            </div>
+  const elements = news.map(article => (
+    <div class="relative mb-4 before:content-[''] before:rounded-md before:absolute before:inset-0 before:bg-black before:bg-opacity-20">
+      <img class="w-full h-full rounded-md" src={article.urlToImage}/>
+      <div class="absolute inset-0 bg-black left-0 top-0 flex flex-col items-center justify-center opacity-0 hover:opacity-60 bg-opacity-90 duration-300">
+        <div class="relative">
+          <a class="test__link absolute inset-0" target="_blank" href={article.url}></a>
+          <h1 class="test__title text-orange-300 text-xs md:text-sm lg:text-xl font-bold mb-3">{article.title}</h1>
+        </div>
       </div>
-  )
-  });
-
-
-  return (
-
-    <>
-
-    <div>
-      {elements}
     </div>
-
-  </>
+  ));
+  
+  return (
+    <>
+      <div>
+        {elements}
+      </div>
+    </>
   );
-
+  
  
 }
 
