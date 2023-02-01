a132783d5f9d53d928b9be76ffcc2d1cce5b6595
don1997 <donald_j97@hotmail.com>
2023-02-01 10:24:57 -0500

    init commit

--- src/App.js
+++ src/App.js
@@ -3,22 +3,14 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
+    <p className="text-3xl text-gray-700 font-bold mb-5">
+      Welcome!
+    </p>
+    <p className="text-gray-500 text-lg">
+      React and Tailwind CSS in action
+    </p>
+  </div>
   );
 }
 
